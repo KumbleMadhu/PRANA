73600a1b1cf8ddf1520aaf8ca86526a7b7fc472a
madatpython <madhukumblecodes@gmail.com>
2020-07-05 00:22:07 +0530

    final script -prana

--- server.py
+++ server.py
@@ -6,17 +6,10 @@ from web3 import Web3
 import web3
 import json
 import http
-import numpy as np
-import pandas as pd
 from flask_cors import CORS
-import numpy as np
-import pandas as pd
 import datetime as dt
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-
-
+import pandas as pd
+from sklearn.cluster import DBSCAN
 
 
 app = flask.Flask(__name__)
@@ -55,6 +48,7 @@ def internal_server_error(e):
 
 @app.route('/api/v0/get_infections', methods=['GET'])
 def get_infections():
+    masterDataSet = []
     query_parameters = request.args
     if 'id' in query_parameters:
         id = query_parameters.get('id')
@@ -62,7 +56,6 @@ def get_infections():
         if getLatestCID(id) == "":
             return page_not_found(404)
         else:
-            # TODO: Find infections
             w3 = Web3(Web3.HTTPProvider(blockchain_url))
             contract = w3.eth.contract(
                 os.environ['PROOF_SMART_CONTRACT_ADDRESS'], abi=abi)
@@ -78,79 +71,56 @@ def get_infections():
                 for location in jsonData:
                     masterDataSet.append(location)
             print("Generated live dataset of length: %d" % len(masterDataSet))
-            with open('live_dataset.json', 'x') as outfile:
-                json.dump(masterDataSet, outfile, indent=2)
+            try:
+                with open('live_dataset.json', 'x') as outfile:
+                    json.dump(masterDataSet, outfile, indent=2)
+            except FileExistsError:
+                os.remove('live_dataset.json')
+                print("File Removed!")
+                with open('live_dataset.json', 'x') as outfile:
+                    json.dump(masterDataSet, outfile, indent=2)
             results = get_infected_ids(id)
             os.remove("live_dataset.json")
+            # masterDataSet = []
             return (jsonify(results))
     else:
         return "Error: Please specify an ID to identify potential infections."
 
 
-def get_infected_ids(id):
-    dtc = train('training_dataset.json')
-    basePath = os.path.dirname(os.path.abspath(__file__))
-    live_dataset_path = basePath + '/' + 'live_dataset.json'
-    X = pd.read_json(live_dataset_path)
-    X = X.sort_values(by=['timestamp'])
-    X['timestamp'] = pd.to_datetime(X['timestamp'].sort_values())
-    X['year'] = X['timestamp'].dt.year
-    X['month'] = X['timestamp'].dt.month
-    X['day'] = X['timestamp'].dt.day
-    X['hour'] = X['timestamp'].dt.hour
-    X['minute'] = X['timestamp'].dt.minute
-    X['second'] = X['timestamp'].dt.second
-    # sep = pd.DataFrame(columns=['infection'])
-    # X
-    # df_total = pd.merge(X,sep[['infection']],how = 'right',left_index = True, right_index = True)
+def get_infected_ids(input_id):
+    basePath = os.path.dirname(os.path.abspath('live_dataset.json'))
+    dflive = pd.read_json(basePath + '/' + 'live_dataset.json')
 
-    # sep.reset_index(level=0, inplace=True)
-    # print(final_df)
-    # final_df = X.merge(sep, on='index', indicator = True)
-    ids = pd.DataFrame(X['id'])
-    timestamps = pd.DataFrame(X['timestamp'])
-    X.drop(['timestamp', 'id'], axis = 1, inplace=True)
+    epsilon = 0.0018288 # a radial distance of 6 feet, which is medically presribed
+    min_sample = 2
+    model = DBSCAN(eps=epsilon, min_samples=2, metric='haversine').fit(dflive[['latitude', 'longitude']])
+    dflive['cluster'] = model.labels_.tolist()
 
-    # X = X.drop(['id'],axis= 1)
-    # y = pd.DataFrame(X['infection'])
-    presub = pd.DataFrame(ids.join(timestamps))
-    sub = pd.DataFrame(presub.join(X))
-    y = dtc.predict(X)
-    print(y)
-    infection = pd.DataFrame(y)
-    final = pd.DataFrame(sub.join(infection))
-    final.rename(columns = {0:'infection'}, inplace=True)
-    final.drop(['year', 'month', 'day', 'hour', 'minute', 'second'], axis = 1, inplace=True)
-    print(final.head())
-    print(final.columns)
-    final.sort_values(by = 'timestamp')
-    # final.drop(['timestamp', 'latitude', 'longitude'], axis = 1)
-
-
-    # print(X.head())
-    # X = pd.get_dummies(X, columns=['id'], drop_first=True)
+    input_id_clusters = []
+    for i in range(len(dflive)):
+        if dflive['id'][i] == input_id:
+            if dflive['cluster'][i] in input_id_clusters:
+                pass
+            else:
+                input_id_clusters.append(dflive['cluster'][i])
+#     print(input_id_clusters)
     
-    
-    # X['infection'] = infection
-    # X.join(infection)
-    print(final.head(50))
-
-    # live_dataset_file = open('live_dataset.json', 'r')
-    # live_dataset_data = json.load(live_dataset_file)
-    # print(live_dataset_data)
-
-    
-    potential_infected_ids = ["0xABC", "0xDEF", "0xGHI", "0xJKL", "0xMNO"]
-    results = {
-        "id": id,
-        "potential_infected_ids": potential_infected_ids
-    }
-    return results
+    infected_ids = []
+    for cluster in input_id_clusters:
+        if cluster != -1:
+            ids_in_cluster = dflive.loc[dflive['cluster'] == cluster, 'id']
+            for i in range(len(ids_in_cluster)):
+                member_id = ids_in_cluster.iloc[i]
+                if (member_id not in infected_ids) and (member_id != input_id):
+                    infected_ids.append(member_id)
+                else:
+                    pass
+    return infected_ids
 
 
 def train(dataset):
     basePath = os.path.dirname(os.path.abspath(__file__))
-    training_dataset_path = basePath + '/' +dataset
+    training_dataset_path = basePath + '/' + dataset
     df = pd.read_json(training_dataset_path)
     df = df.sort_values(by=['timestamp'])
     df['timestamp'] = pd.to_datetime(df['timestamp'].sort_values())
@@ -160,18 +130,18 @@ def train(dataset):
     df['hour'] = df['timestamp'].dt.hour
     df['minute'] = df['timestamp'].dt.minute
     df['second'] = df['timestamp'].dt.second
-    df.drop(['timestamp', 'id'], axis = 1, inplace=True)
-    # df = pd.get_dummies(df, columns=['id'], drop_first=True)
+    df.drop(['timestamp', 'id'], axis=1, inplace=True)
 
-    X = df.drop(['infection'],axis= 1)
+    X = df.drop(['infection'], axis=1)
     y = df['infection']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=101)
     dtc = DecisionTreeClassifier(criterion='gini', splitter='best')
     dtc.fit(X_train, y_train)
     pred = dtc.predict(X_test)
     print(classification_report(pred, y_test))
     print(confusion_matrix(pred, y_test))
-    print('Accuracy score:\n',accuracy_score(y_test, pred))
+    print('Accuracy score:\n', accuracy_score(y_test, pred))
     return dtc
 
 
